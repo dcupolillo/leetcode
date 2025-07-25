6945307845f7f9d84bcb4c0c0fc98c6c555d2720
dcupolillo <dario.cupolillo@gmail.com>
2025-07-25 10:44:55 +0200

    Add solution for "Remove Element" problem and "Merge Sorted Arrays"

---
+++ 27. Remove Element/27.remove-element.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        if len(nums) < 0 and len(nums) > 100:
+            raise ValueError("Length of `nums` should be included between 0-100.")
+
+        if val < 0 and val > 100:
+            raise ValueError("`val` should be between 0-100.")
+
+        if sum([True for num in nums if num < 0 and num > 50]) > 0:
+            raise ValueError("Values in `nums` should be between 0-50.")
+        
+        indices = [i for i, num in enumerate(nums) if num == val]
+
+        k = len([v for v in nums if v != val])
+        for index in sorted(indices, reverse=True):
+            del nums[index]
+
+        return k
+        
+# @lc code=end
+
---
+++ 88. Merge Sorted Array/88.merge-sorted-array.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+import numpy as np
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        assert len(nums1) == m + n, f"nums1 should have a lenght of {m + n}"
+        assert len(nums2) == n, f"nums2 should have a lenght of n = {n}"
+        if m < 0:
+            raise ValueError("m should be >= 0.")
+        if n > 200:
+            raise ValueError ("n should be <= 200.")
+        if (m + n) < 1 and (m + n) > 200:
+            raise ValueError("m + n should be included between 1 and 200.")
+
+        if m == 0:
+            del nums1[:]
+        if n!= 0:
+            del nums1[m:]
+        
+        nums1.extend(nums2[:n])
+        nums1.sort()
+
+
+        
+        
+# @lc code=end
+
