60538cf52db513c399cf462134ab4568c896aec8
dcupolillo <dario.cupolillo@gmail.com>
2025-07-25 13:51:52 +0200

    Add solution for "Two Sum" problem

---
+++ 1. Two Sum/1.two-sum.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(
+            self,
+            nums: list,
+            target: int
+    ) -> list:
+        """
+        Given an array of integers nums and an integer target,
+        return indices of the two numbers such that they add up to target.
+        
+        Notes
+        ------
+        You may assume that each input would have exactly one solution,
+        and you may not use the same element twice.
+        """
+
+        # Costraints
+        if len(nums) < 2 or len(nums) > 10**4:
+            raise ValueError("Length of `nums` should be between 2 and 10^4.")
+       
+        if target < -10**9 or target > 10**9:
+            raise ValueError("`target` should be between -10^9 and 10^9.")
+        
+        for num in nums:
+            if num < -10**9 or num > 10**9:
+                raise ValueError("Values in `nums` should be between -10^9 and 10^9.")
+        
+        if len(nums) == 2:
+            return [0, 1]
+        
+        for n_row, row in enumerate(nums):
+            for n_col, col in enumerate(nums):
+                if n_col <= n_row:
+                    continue
+        
+                if row + col == target and n_row != n_col:
+                    return [n_row, n_col]
+                    
