5b16976903c79a2fba709b1d9f872cc9a810c2e2
dcupolillo <dario.cupolillo@gmail.com>
2025-07-25 16:18:01 +0200

    Add solution for "Longest Common Prefix" problem

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(
+            self,
+            strs: list
+    ) -> str:
+
+        common_prefix = ""
+
+        shortest_string = min(strs, key=len)
+        
+        for n_char, char in enumerate(shortest_string):
+            
+            if not all(s[n_char] == char for s in strs):
+                break
+            
+            common_prefix += char
+
+        return common_prefix
+# @lc code=end
+
